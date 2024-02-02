332fb647224709725f1ba009d901d0268e9f2ee0
Chandan-devs-tech <chandanguptabsb@gmail.com>
2024-02-02 11:21:39 +0530

    Modify Details.jsx

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -12,7 +12,32 @@ const Details = () => {
   }, [id]);
 
   if (!showDetails) {
-    return <div>Loading...</div>;
+    return (
+      <div className="flex justify-center items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon icon-tabler icon-tabler-loader text-teal-500"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          strokeWidth="4"
+          stroke="currentColor"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M12 6l0 -3" />
+          <path d="M16.25 7.75l2.15 -2.15" />
+          <path d="M18 12l3 0" />
+          <path d="M16.25 16.25l2.15 2.15" />
+          <path d="M12 18l0 3" />
+          <path d="M7.75 16.25l-2.15 2.15" />
+          <path d="M6 12l-3 0" />
+          <path d="M7.75 7.75l-2.15 -2.15" />
+        </svg>
+      </div>
+    );
   }
 
   return (
